de4716941843868c81386edcebc530b084bbac8e
2292388933 <2292388933@qq.com>
2021-01-19 15:15:06 +0800

    none

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from 'react';
 
 function App() {
+  const[musicUrl,setMusicUrl] =useState("");
+  const[text,setText]= useState("");
+
+  useEffect(() => {
+    const fn = async()=>{
+    const res = await fetch(`http://139.196.141.233:3000/song/url?id=33894312`);
+    const data = await res.json();
+    console.log (data);
+    console.log (data.data[0].url);
+    setMusicUrl(data.data[0].url);
+  }
+    fn();
+  }, [0])
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +31,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+
+</div>
+<input onChange={(event)=>{setText(event.target.value)}}></input>
       </header>
+      <audio autoPlay src={musicUrl}></audio>
     </div>
   );
 }
